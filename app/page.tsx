52b75a0f17ab9c1bf511b0fb2a28ce2db3e8241a
Chirag Goyal <chirag.goyal777@gmail.com>
2025-06-11 06:08:16 +0530

    prettier configured + formatting

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
   const { setTheme } = useTheme();
 
